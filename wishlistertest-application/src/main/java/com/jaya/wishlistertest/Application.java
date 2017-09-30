8f9270963d58642920d3cbd75b3e996656f624a1
Caroline Lopes <caroline_clopes@hotmail.com>
2017-09-30 12:09:02 -0300

    Initial commit.

---
+++ wishlistertest-application/src/main/java/com/jaya/wishlistertest/Application.java
@@ -0,0 +1,15 @@
+package com.jaya.wishlistertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Caroline Lopes on 30/09/17.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String args[]) {
+        SpringApplication.run(Application.class, args);
+    }
+}
